378953ab240d301dbf725cba70712f3ccbde519c
Alyssa Lim <alyssatl@student.unimelb.edu.au>
2025-06-05 02:52:41 +1000

    layout adjustments, size pop up,

--- product.js
+++ product.js
@@ -39,18 +39,38 @@ window.addEventListener('DOMContentLoaded', function() {
     // Grab the pop-up element
     var popUp = document.getElementById('added-pop-up');
     var closeBtn = document.getElementById('close-pop-up');
+    var sizeSelect   = document.getElementById('size');
+    var sizeWarning  = document.getElementById('size-warning');
+    var closeWarnBtn = document.getElementById('close-size-warning');
 
     // Find all "add to cart" buttons on the product page
     var addButtons = document.getElementsByClassName('add-to-cart');
 
-    // Show pop-up when "add to cart" button is clicked, disappears after 3 seconds
+    // Functions to show and hide size warning pop up
+    function showSizeWarning() {
+        sizeWarning.classList.add('active');
+    }
+    closeWarnBtn.addEventListener('click', function() {
+        sizeWarning.classList.remove('active');
+    });
+
+    /* When "add to cart" button is clicked, if size is not selected, show size warning pop up.
+       If not show "added to cart" pop-up, both pop ups disappear after 3 seconds */
     for (var i = 0; i < addButtons.length; i++) {
-      addButtons[i].addEventListener('click', function() {
-        popUp.classList.add('active');
-        setTimeout(function() {
-            popUp.classList.remove('active');
-          }, 3000);
-      });
+        addButtons[i].addEventListener('click', function(e) {
+            if (!sizeSelect.value) {
+                e.preventDefault();
+                showSizeWarning();
+                setTimeout(function() {
+                    sizeWarning.classList.remove('active');
+                }, 3000);
+                return;
+            }
+            popUp.classList.add('active');
+            setTimeout(function() {
+                popUp.classList.remove('active');
+            }, 3000);
+        });
     }
 
     if (closeBtn) {
@@ -60,13 +80,25 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-// Cart functions
+// Open and closing cart
 window.addEventListener('DOMContentLoaded', function() {
-    var cartBtn = document.getElementsByClassName('cart-button');
+    var cartBtn = document.getElementById('cart-button');
+    var addAndCheckout = document.getElementsByClassName('add-and-checkout');
     var overlay = document.getElementById('cart-overlay');
     var blur = document.getElementById('blur');
     var sideCart = document.getElementById('side-cart');
     var closeBtn = document.getElementById('close-cart');
+    var sizeSelect = document.getElementById('size');
+    var sizeWarning = document.getElementById('size-warning');
+    var closeWarnBtn = document.getElementById('close-size-warning');
+
+    // Functions to show and hide size warning pop up
+    function showSizeWarning() {
+        sizeWarning.classList.add('active');
+    }
+    closeWarnBtn.addEventListener('click', function() {
+        sizeWarning.classList.remove('active');
+    });
     
     // Functions to open/close the cart
     function openCart() {
@@ -84,6 +116,21 @@ window.addEventListener('DOMContentLoaded', function() {
     // Show cart when cart button is clicked
     for (var i = 0; i < cartBtn.length; i++) {
         cartBtn[i].addEventListener('click', openCart);
+    }
+
+    // Also show cart when .add-to-checkout button is clicked, unless size is not selected
+    for (var j = 0; j < addAndCheckout.length; j++) {
+        addAndCheckout[j].addEventListener('click', function(e) {
+            if (!sizeSelect.value) {
+                e.preventDefault();
+                showSizeWarning();
+                setTimeout(function() {
+                    sizeWarning.classList.remove('active');
+                }, 3000);
+                return;
+            }
+            openCart();
+        });
     }
     
     // Close cart when X button is clicked
